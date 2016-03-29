16c33270b011928d594abccaa0cf631d4b741b7f
Joshua Van Blake <josh@vanblake.com>
2016-03-29 15:27:48 -0700

    remove testing code.

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/user', function(req, res){
-  res.status(200).json({ name: 'tobi' });
-});
-
 app.listen(3000, function () {
   console.log('Listening on port:3000');
 });
