2632026480bc1fdbcd6eb2f77c5da4dc552306b6
Joshua Van Blake <josh@vanblake.com>
2016-03-29 14:31:01 -0700

    adding server test.

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('./public'));
+app.use(express.static(__dirname + '/public'));
 
+app.get('/user', function(req, res){
+  res.status(200).json({ name: 'tobi' });
+});
 
 app.listen(3000, function () {
   console.log('Listening on port:3000');
 });
+
+
+module.exports = app;
--- spec/serverSpec.js
+++ spec/serverSpec.js
@@ -1,8 +1,27 @@
+var request = require('supertest');
 var expect = require('expect');
+var app = require("../index.js");
 
-describe("Mocha Test", function(){
-  it("should pass", function(){
-    expect(true);
+describe("Server Test", function(){
+  it("index should be served", function(done){
+    request(app)
+      .get('/')
+      .set('Accept', 'text/HTML')
+      .expect(200)
+      .expect( function(res){
+        expect(res.text).toContain("Hello QuoteBoard.");
+      })
+      .end(done);
+  });
+
+  it("style ", function(done){
+    request(app)
+      .get('/style.css')
+      .set('Accept', 'text/HTML')
+      .expect(200)
+      .end(done);
   });
 
 });
+
+ 
